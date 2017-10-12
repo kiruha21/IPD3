907a06b6a98c11e30de8b9302faee79bf73b0873
kiruha21 <vsegda_online_007@bk.ru>
2017-10-12 23:09:00 +0300

    Revert "121017"
    This reverts commit 51c0792d7c3fafb7ed3a76b1e97fafb1dfce1521.

--- app/src/main/java/com/yarashevich/kiryl/ipd/MainActivity.java
+++ app/src/main/java/com/yarashevich/kiryl/ipd/MainActivity.java
@@ -3,9 +3,7 @@ package com.yarashevich.kiryl.ipd;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -25,17 +22,6 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.intern);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.uoipd.by/ru/news/" +
-                        ""));
-                startActivity(browserIntent);
-                Snackbar.make(view, "Переход на сайт: uoipd.by новости", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
