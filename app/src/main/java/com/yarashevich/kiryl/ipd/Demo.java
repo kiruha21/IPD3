831bc2050947b424bedbdf1b132b9ece28b1f4ea
kiruha21 <vsegda_online_007@bk.ru>
2018-01-18 17:58:07 +0300

    1.10.24-YK20180118V-beta1
    1.10.24-YK20180118V-beta1

--- app/src/main/java/com/yarashevich/kiryl/ipd/Demo.java
+++ app/src/main/java/com/yarashevich/kiryl/ipd/Demo.java
@@ -23,7 +23,7 @@ public class Demo {
 
         appCardBuilder.addItem(new MaterialAboutTitleItem.Builder()
                 .text("О программе:")
-                .desc("© 2017 Kirill Yaroshevich")
+                .desc("© 2016 Kirill Yaroshevich")
                 .icon(R.mipmap.kkkk)
                 .build());
 
